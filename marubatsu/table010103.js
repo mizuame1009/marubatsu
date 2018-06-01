5b980228685668229db1f61c69d23a5bf30a6fde
Palaeoloxodon <aptonynlus@gmail.com>
2018-06-01 19:14:47 +0900

    index.html

---
+++ marubatsu/table010103.js
@@ -0,0 +1,160 @@
+var num_max = 39;
+var mondai = [];
+var seitou = [];
+var kaitou = [];
+
+mondai[1] = "電磁波のエネルギーEは振動数νの関数で、E=hνで表される。";
+seitou[1] = 'm';
+kaitou[1] = "";
+
+mondai[2] = "電磁波の真空中での速度はいずれも同じである";
+seitou[2] = 'm';
+kaitou[2] = "電磁波の真空中での速度は光の伝播速度と等しく、3×10<sup>8</sup>m・s<sup>-1</sup>です。";
+
+mondai[3] = "紫外線、可視光線、赤外線の順に、波長も振動数も共に大きくなる。";
+seitou[3] = 'b';
+kaitou[3] = "波長は紫外線<可視光線<赤外線の順に大きくなるが、振動数は逆になる。";
+
+mondai[4] = "核磁気共鳴スペクトル測定法に利用されるラジオ波は、X線よりも波長が短い電磁波である。";
+seitou[4] = 'b';
+kaitou[4] = "ラジオ波はX線よりも波長が長い。";
+
+mondai[5] = "吸光度Aは透過度tの逆数の常用対数である。";
+seitou[5] = 'm';
+kaitou[5] = "";
+
+mondai[6] = "無極性分子は、マイクロ波分光法により回転準位間の遷移を観察することはできない。";
+seitou[6] = 'm';
+kaitou[6] = "";
+
+mondai[7] = "すべての分子振動は赤外線により励起される。";
+seitou[7] = 'b';
+kaitou[7] = "分子振動により電気双極子モーメントに変化が生じるもののみ赤外活性となる。";
+
+mondai[8] = "二酸化炭素は対象伸縮性による赤外線の吸収が観測されない。";
+seitou[8] = 'm';
+kaitou[8] = "";
+
+mondai[9] = "紫外線及び可視光線の吸収は、電子遷移に起因する。";
+seitou[9] = 'm';
+kaitou[9] = "";
+
+mondai[10] = "一般に、吸収スペクトルと発光スペクトルの振動構造は鏡像関係にある。";
+seitou[10] = 'm';
+kaitou[10] = "";
+
+mondai[11] = "電子スピン共鳴法は";
+seitou[11] = '';
+kaitou[11] = "";
+
+mondai[12] = "";
+seitou[12] = '';
+kaitou[12] = "";
+
+mondai[13] = "";
+seitou[13] = '';
+kaitou[13] = "";
+
+mondai[14] = "";
+seitou[14] = '';
+kaitou[14] = "";
+
+mondai[15] = "";
+seitou[15] = '';
+kaitou[15] = "";
+
+mondai[16] = "";
+seitou[16] = '';
+kaitou[16] = "";
+
+mondai[17] = "";
+seitou[17] = '';
+kaitou[17] = "";
+
+mondai[18] = "";
+seitou[18] = '';
+kaitou[18] = "";
+
+mondai[19] = "";
+seitou[19] = '';
+kaitou[19] = "";
+
+mondai[20] = "";
+seitou[20] = '';
+kaitou[20] = "";
+
+mondai[21] = "";
+seitou[21] = '';
+kaitou[21] = "";
+
+mondai[22] = "";
+seitou[22] = '';
+kaitou[22] = "";
+
+mondai[23] = "";
+seitou[23] = '';
+kaitou[23] = "";
+
+mondai[24] = "";
+seitou[24] = '';
+kaitou[24] = "";
+
+mondai[25] = "";
+seitou[25] = '';
+kaitou[25] = "";
+
+mondai[26] = "";
+seitou[26] = '';
+kaitou[26] = "";
+
+mondai[27] = "";
+seitou[27] = '';
+kaitou[27] = "";
+
+mondai[28] = "";
+seitou[28] = '';
+kaitou[28] = "";
+
+mondai[29] = "";
+seitou[29] = '';
+kaitou[29] = "";
+
+mondai[30] = "";
+seitou[30] = '';
+kaitou[30] = "";
+
+mondai[31] = "";
+seitou[31] = '';
+kaitou[31] = "";
+
+mondai[32] = "";
+seitou[32] = '';
+kaitou[32] = "";
+
+mondai[33] = "";
+seitou[33] = '';
+kaitou[33] = "";
+
+mondai[34] = "";
+seitou[34] = '';
+kaitou[34] = "";
+
+mondai[35] = "";
+seitou[35] = '';
+kaitou[35] = "";
+
+mondai[36] = "";
+seitou[36] = '';
+kaitou[36] = "";
+
+mondai[37] = "";
+seitou[37] = '';
+kaitou[37] = "";
+
+mondai[38] = "";
+seitou[38] = '';
+kaitou[38] = "";
+
+mondai[39] = "";
+seitou[39] = '';
+kaitou[39] = "";
